8d6f5b386131a7b46fc907759c41e96e96c37425
hristo-W3 <hristo_valeriev@yahoo.com>
2026-05-28 21:32:37 +0300

    Create solve.js

---
+++ JS Advanced/2026/JS Advanced - May26/01. Syntax, Functions and Statements/Lab/03. Largest Number/solve.js
@@ -0,0 +1,8 @@
+function largestNumber(num1, num2, num3) {
+    let largest = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largest}.`);
+}   
+
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
+largestNumber(5, 3, 16);
