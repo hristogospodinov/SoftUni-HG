3b0e2d3ee97de84af9aa253c6eab4b5c6b4eefb1
hristo-W3 <hristo_valeriev@yahoo.com>
2026-05-28 21:26:20 +0300

    Update echo.js

---
+++ JS Advanced/2026/JS Advanced - May26/01. Syntax, Functions and Statements/Lab/01. Echo Function/echo.js
@@ -0,0 +1,9 @@
+function echo(input) {
+    console.log(input.length);
+    console.log(input);
+};
+
+echo('Hello, JavaScript!');
+echo('strings are easy');
+echo('12345');
+echo('This is a longer string that should also work correctly.');
