21081de989049f71afc3b35674e7ba4fce79a822
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-02 00:47:32 +0300

    Create day.js

---
+++ JS Advanced/2026/JS Advanced - May26/01. Syntax, Functions and Statements/Exercise/04. Previous Day/day.js
@@ -0,0 +1,9 @@
+function prevDay(year, month, day) {
+    let date = new Date(year, month - 1, day);
+    date.setDate(date.getDate() - 1);
+    return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;    
+}
+
+// Tests
+console.log(prevDay(2016, 9, 30));      
+console.log(prevDay(2015, 5, 10));
