e1dc13dfce409cd5c2666beca43f14ec434df1a8
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-08 13:25:21 +0300

    Create solve.js

---
+++ JS Advanced/2026/JS Advanced - May26/02. Arrays and Nested Arrays/Lab/04. Negative - Positive Numbers/solve.js
@@ -0,0 +1,15 @@
+function polarArrays(arr) {
+    let result = [];
+    for (let num of arr) {
+        if (num < 0) {
+            result.unshift(num);
+        } else {
+            result.push(num);
+        }        
+    }
+    console.log(result.join('\n'));
+}
+
+polarArrays([7, -2, 8, 9]);
+console.log('-------------------');
+polarArrays([3, -2, 0, -1])
