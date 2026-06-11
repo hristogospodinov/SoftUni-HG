2d2e67990add5e109ac1d3b5d164920ab6a8d155
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-11 16:39:39 +0300

    Create solve.js

---
+++ JS Advanced/2026/JS Advanced - May26/02. Arrays and Nested Arrays/Exercise/01. Print an Array/solve.js
@@ -0,0 +1,22 @@
+function printArr(array, str) {
+    console.log(array.join(str));
+    
+}
+
+printArr(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+)
+
+console.log('-'.repeat(20));
+printArr(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+)
