9043a6b3f9858b3bbf445b525063cbbfbdba11a0
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-11 20:53:21 +0300

    Update solve.js

---
+++ JS Advanced/2026/JS Advanced - May26/02. Arrays and Nested Arrays/Exercise/04. Rotate Array/solve.js
@@ -0,0 +1,22 @@
+function arrRotate(arr, steps) {
+    for (let i = 0; i < steps; i++) {
+        let last = arr.pop();
+        arr.unshift(last);
+    }
+    console.log(arr.join(' '));
+    
+}
+
+arrRotate(['1', 
+'2', 
+'3', 
+'4'], 
+2
+)
+
+arrRotate(['Banana', 
+'Orange', 
+'Coconut', 
+'Apple'], 
+15
+)
