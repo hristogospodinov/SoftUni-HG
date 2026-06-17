2443e91e039effd6f5fc064df62fd4b950ff2ab3
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-17 15:28:34 +0300

    Create solve.js

---
+++ JS Advanced/2026/JS Advanced - May26/03. Objects and Composition/Exercise/06. Store Catalogue/solve.js
@@ -0,0 +1,41 @@
+function storage(input) {
+    input.sort((a, b) => a.localeCompare(b));
+
+    let currentLetter = '';
+
+    for (const product of input) {
+        let [name, price] = product.split(' : ');
+
+        let firstLetter = name[0];
+        if (firstLetter != currentLetter) {
+            console.log(firstLetter);
+            currentLetter = firstLetter;            
+        }
+        console.log(`  ${name}: ${price}`);
+        
+    }
+    
+    
+}
+
+storage(['Appricot : 20.4',
+'Fridge : 1500',
+'TV : 1499',
+'Deodorant : 10',
+'Boiler : 300',
+'Apple : 1.25',
+'Anti-Bug Spray : 15',
+'T-Shirt : 10']
+);
+
+console.log('-'.repeat(20));
+
+storage(["Banana : 2",
+"Rubic's Cube : 5",
+"Raspberry P : 4999",
+"Rolex : 100000",
+"Rollon : 10",
+"Rali Car : 2000000",
+"Pesho : 0.000001",
+"Barrel : 10"]
+);
