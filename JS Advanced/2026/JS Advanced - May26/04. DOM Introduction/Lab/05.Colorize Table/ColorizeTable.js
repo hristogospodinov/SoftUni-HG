97ddaa220cce76d49e3cd25ad207122ea705e84b
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 15:28:50 +0300

    Update ColorizeTable.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    const elements = document.querySelectorAll('tr');
+
+    for (let i = 0; i < elements.length; i++) {
+        if (i % 2 == 1) {
+            elements[i].style.backgroundColor = 'Teal';
+        }
+    }
 }
