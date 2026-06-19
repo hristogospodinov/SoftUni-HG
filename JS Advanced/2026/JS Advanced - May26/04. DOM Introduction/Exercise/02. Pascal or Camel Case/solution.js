d12d03dcb4d9a35dd49259a74b211398e6a75dfe
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 18:51:45 +0300

    Update solution.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let words = document.getElementById('text').value.toLowerCase();
+  let currentCase = document.getElementById('naming-convention').value;
+  let result = '';
+
+  words = words.split(" ");
+  console.log(words[0][0]);
+  
+  if (currentCase.toLowerCase() === "camel case") {
+    result += words[0];
+    for (let i = 1; i < words.length; i++) {
+      result += words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+    }
+  } else if (currentCase.toLowerCase() === "pascal case") {
+    for (let i = 0; i < words.length; i++) {
+      result += words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+    }
+  } else {
+      result += "Error!";
+  }
+
+  document.getElementById('result').textContent = result;
+    
 }
