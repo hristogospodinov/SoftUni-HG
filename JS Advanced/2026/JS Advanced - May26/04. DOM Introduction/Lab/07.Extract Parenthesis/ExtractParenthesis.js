254543d4e11742d9d25b90909ac22bff51f5946b
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 15:58:27 +0300

    Update ExtractParenthesis.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,9 @@
 function extract(content) {
+    let contentFull = document.getElementById(content).textContent;
 
+    return contentFull.match(/\(.*?\)/g)
+        .map(a => a.slice(1, -1))
+        .join("; ");
 }
+
+// let text = extract("content");
