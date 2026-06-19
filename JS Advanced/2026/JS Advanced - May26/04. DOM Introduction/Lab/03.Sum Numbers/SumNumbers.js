c93e733017c6fcf91596d3a155bf9e547796bc2d
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 15:03:24 +0300

    Update SumNumbers.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,9 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const number1 = Number(document.getElementById('num1').value);
+    const number2 = Number(document.getElementById('num2').value);
+
+    const resultArea = document.getElementById('sum');
+    resultArea.value = number1 + number2;
+
+
 }
