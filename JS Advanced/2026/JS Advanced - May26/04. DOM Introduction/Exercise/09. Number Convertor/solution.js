fd575cee6df46572d59577a74cc03e814814b999
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 21:18:15 +0300

    Update solution.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    const selectMenuTo = document.getElementById('selectMenuTo');
 
-    //TODO...
+    const binaryOption = document.createElement('option');
+    binaryOption.value = 'binary';
+    binaryOption.textContent = 'Binary';
+
+    const hexadecimalOption = document.createElement('option');
+    hexadecimalOption.value = 'hexadecimal';
+    hexadecimalOption.textContent = 'Hexadecimal';
+
+    selectMenuTo.appendChild(binaryOption);
+    selectMenuTo.appendChild(hexadecimalOption);
+
+    document.querySelector('button').addEventListener('click', convert);
+
+    function convert() {
+        const number = Number(document.getElementById('input').value);
+        const type = selectMenuTo.value;
+        const result = document.getElementById('result');
+
+        if (type === 'binary') {
+            result.value = number.toString(2);
+        } else if (type === 'hexadecimal') {
+            result.value = number.toString(16).toUpperCase();
+        }
+    }
 }
