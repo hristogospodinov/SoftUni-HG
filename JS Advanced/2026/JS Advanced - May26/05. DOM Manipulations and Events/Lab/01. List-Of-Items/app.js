6f1b3e88d0bb930db249070f6538089bf853bd82
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 22:44:18 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/01. List-Of-Items/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/01. List-Of-Items/app.js
@@ -1,9 +1,15 @@
 function addItem() {
     const input = document.getElementById('newItemText');
 
+    if (!input.value) {
+        return;
+    }
+
     const item = document.createElement('li');
     item.textContent = input.value;
 
     const list = document.getElementById('items');
     list.appendChild(item);
+
+    input.value = '';
 }
