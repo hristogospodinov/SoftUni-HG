551fb992f6b44c5044e9372d25274ba0890b2f47
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 19:43:25 +0300

    Update solution.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/06. Format the Text/solution.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+    const input = document.getElementById('input').value;
+    const output = document.getElementById('output');
+
+    const sentences = input
+        .split('.')
+        .filter(sentence => sentence.trim() !== '');
+
+    let result = '';
+
+    for (let i = 0; i < sentences.length; i += 3) {
+        const paragraphText = sentences
+            .slice(i, i + 3)
+            .join('. ') + '.';
+
+        result += `<p>${paragraphText}</p>`;
+    }
+
+    output.innerHTML = result;
 }
