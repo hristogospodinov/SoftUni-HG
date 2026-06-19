bfc01919df3aa83ddd40351c77a1b18e4fecf604
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 23:01:35 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,25 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const input = document.querySelector('[name="email"]');
+    const pattern = input.value;
+    const result = document.getElementById('result');
+
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let found = false;
+
+    for (let row of rows) {
+        if (row.children[1].textContent == pattern) {
+            row.remove();
+            found = true;
+        } 
+    }   
+
+    if (found) {
+        result.textContent = "Deleted.";
+        input.value = "";
+    } else {
+        result.textContent = "Not found.";
+    }
+
+    
 }
