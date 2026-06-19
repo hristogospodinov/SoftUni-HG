62348cfd53fd1e7b345b2fb7ca463b9209edf490
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 15:44:45 +0300

    Update SumTable.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+    let table = document.querySelectorAll('table tr');
+    let totalField = document.getElementById('sum');
+    let total = 0;
 
+    
+
+    for(let i = 1; i < table.length - 1; i++) {
+        total += Number(table[i].children[table[i].children.length - 1].textContent);
+    }
+
+    totalField.textContent = total;
 }
