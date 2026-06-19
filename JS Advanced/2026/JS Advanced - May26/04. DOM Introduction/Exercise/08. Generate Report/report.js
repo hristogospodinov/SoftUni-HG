3f81f0e5e683b8d0fc39b6bc38e1164c5174a4ad
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 21:16:59 +0300

    Update report.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/08. Generate Report/report.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,30 @@
 function generateReport() {
-    //TODO
+    const headers = document.querySelectorAll('th input');
+
+    let selectedColumns = [];
+
+    headers.forEach((header, index) => {
+        if(header.checked) {
+            selectedColumns.push({
+                name: header.name,
+                index
+            });
+        }
+    });
+
+    const rows = document.querySelectorAll('tbody tr');
+
+    const result = [];
+
+    for (const row of rows) {
+        const cells = row.querySelectorAll('td');
+        const currentRow = {};
+
+        for (const column of selectedColumns) {
+            currentRow[column.name] = cells[column.index].textContent;
+        }
+        result.push(currentRow);
+    }
+
+    document.getElementById('output').value = JSON.stringify(result);
 }
