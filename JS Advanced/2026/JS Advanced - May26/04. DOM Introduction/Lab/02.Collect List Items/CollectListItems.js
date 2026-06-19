fef33c84faf5b82f8b1b37b51059120ee5a08478
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-19 14:53:26 +0300

    Update CollectListItems.js

--- JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ JS Advanced/2026/JS Advanced - May26/04. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    const items = document.querySelectorAll('li');
+    const resultArea = document.getElementById('result');
+
+    const text = Array.from(items)
+        .map(item => item.textContent)
+        .join('\n');
+
+    resultArea.value = text;
 }
