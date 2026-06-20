c064c0620484dd28a49a5731f758e02b6e4c07b6
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-20 06:23:39 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const output = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', onMouseMove);
+    gradient.addEventListener('mouseout', onMouseOut);
+
+    function onMouseMove(event) {
+        output.textContent = Math.floor(event.offsetX / event.target.clientWidth * 100) + "%";
+    }
+
+    function onMouseOut(event) {
+        output.textContent = '';        
+    }
+
 }
