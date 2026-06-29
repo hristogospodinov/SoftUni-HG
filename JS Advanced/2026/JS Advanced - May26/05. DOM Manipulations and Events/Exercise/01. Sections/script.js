c5343ad2ae481850ff8a8c479739ea23a0f4c704
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-29 21:57:14 +0300

    Update script.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.getElementById('content');
+   for(let word of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+      content.appendChild(div);
+      
+      div.addEventListener('click', show);
+   }
+
+   function show(event) {
+      event.currentTarget.children[0].style.display = "block";
+   }
 }
