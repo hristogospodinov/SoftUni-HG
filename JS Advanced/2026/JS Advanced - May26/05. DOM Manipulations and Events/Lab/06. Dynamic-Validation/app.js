403de329e7e0225a2afc593518df31535e1fc9fe
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-29 20:45:46 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    let mail = document.getElementById('email')
+    mail.addEventListener('change', validFunc)
+    let regX= /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+    function validFunc() {
+        if (!regX.test(this.value)) { 
+            this.classList.add('error'); 
+        } else {
+            this.classList.remove('error'); 
+        }
+    }
+    
 }
