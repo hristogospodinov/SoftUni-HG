0f6435d9b5b3122d61e450c47b130effe5ae28ea
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-29 22:49:57 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('.profile button');
+
+    for(let button of buttons) {
+        button.addEventListener('click', showMore);
+    }
+
+    function showMore(event) {
+        let profile = event.target.parentElement;
+        if (profile.querySelector('[value="unlock"]').checked) {
+            let hidden = profile.querySelector('div');
+            if (event.target.textContent === "Show more") {
+                hidden.style.display = "block";
+                event.target.textContent = "Hide it";
+            } else {
+                hidden.style.display = "none";
+                event.target.textContent = "Show more";
+            }
+        }
+    }
 }
