7055efa5dfb82e3244081f1365b882f0ab8ade7d
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-29 23:36:26 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,24 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let [encodeBox, decodeBox] = document.querySelectorAll('#main textarea');
+    let [encodeButton, decodeButton] = document.querySelectorAll('#main button');
+
+    encodeButton.addEventListener("click", encodeText);
+    decodeButton.addEventListener("click", decodeText);
+
+    function encodeText(event) { 
+        decodeBox.value = shiftMessage(encodeBox.value, 1);
+        encodeBox.value = '';
+    }
+
+    function decodeText(event) {
+        decodeBox.value = shiftMessage(decodeBox.value, -1);
+    }
+
+    function shiftMessage(text, asciicode) {
+        let resultText = '';
+        for(let i = 0; i < text.length; i++) {
+            resultText += String.fromCharCode(text.charCodeAt(i) + asciicode);
+        }
+        return resultText;
+    }
 }
