00c5369cada59244a2894b25877972e585a26b77
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-29 20:26:16 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = document.querySelectorAll('input');
+
+    for (let el of inputs) {
+        el.addEventListener('focus', highlight);
+        el.addEventListener('blur', clear);
+    }
+
+    function highlight() {
+        this.parentElement.classList.add('focused');
+    }
+
+    function clear() {
+        this.parentElement.classList.remove('focused');
+    }
+    
 }
