15dca28f1d35cf02c9945f86258649e70b1d9a63
hristo-W3 <hristo_valeriev@yahoo.com>
2026-06-29 23:03:29 +0300

    Update dropdown.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let list = document.getElementById('menu');
+    let newItemField = document.getElementById('newItemText');
+    let newValueField = document.getElementById('newItemValue');
+
+    let option = document.createElement('option');
+    option.textContent = newItemField.value;
+    option.value = newValueField.value;
+    list.appendChild(option);
+    newItemField.value = '';
+    newValueField.value = '';    
 }
