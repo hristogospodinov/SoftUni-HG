e1de7dc5cbfcdef21fc01bbd5f8be41b810fb203
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-02 20:26:41 +0300

    Update distanceConverter.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,27 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let btn = document.getElementById('convert');
+    btn.addEventListener('click', doConversion);
+    
+    let outputField = document.getElementById('outputDistance');
+
+    let conversionRates = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+    function doConversion() {
+        let originMeasure = document.getElementById('inputUnits').value;
+        let targetMeasure = document.getElementById('outputUnits').value;
+        let originDistance = document.getElementById('inputDistance').value;
+
+        let resultInMeters = conversionRates[originMeasure] * Number(originDistance);
+        let result = resultInMeters / conversionRates[targetMeasure];
+        outputField.value = result;
+    }
 }
