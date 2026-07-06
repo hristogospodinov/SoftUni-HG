3b0f105d4b74b5af7afdd0fdeb65e2c12a08285d
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-06 23:18:44 +0300

    Update solution.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,47 @@
 function solve() {
-  //TODO...
+  let btns = document.querySelectorAll('li.quiz-answer');
+  
+  for(let btn of btns) {
+    btn.addEventListener('click', checkAndContinue);
+  }
+
+  let score = 0;
+  let currentQuestion = 0;
+
+  let correctAnswers = [
+    "onclick",
+    "JSON.stringify()",
+    "A programming API for HTML and XML documents"
+  ]
+
+  function checkAndContinue(event) {
+    let answer = event.currentTarget.querySelector("p").textContent;
+
+    if (answer === correctAnswers[currentQuestion]) {
+        score++;
+    }
+
+    let currentSection = event.currentTarget.parentElement.parentElement;
+
+    currentQuestion++;
+
+    if (currentQuestion === correctAnswers.length) {
+        currentSection.style.display = "none";
+
+        document.getElementById("results").style.display = "block";
+
+        if (score === correctAnswers.length) {
+            document.querySelector("#results h1").textContent =
+                "You are recognized as top JavaScript fan!";
+        } else {
+            document.querySelector("#results h1").textContent =
+                `You have ${score} right answers`;
+        }
+    } else {
+        let nextSection = currentSection.nextElementSibling;
+
+        currentSection.style.display = "none";
+        nextSection.style.display = "block";
+    }
+  }
 }
