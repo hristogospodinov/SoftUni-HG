a69e6933eec37b2ae99c10b796773eba58f91c25
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-06 23:19:43 +0300

    Update solution.js

--- JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ JS Advanced/2026/JS Advanced - May26/05. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -8,40 +8,38 @@ function solve() {
   let score = 0;
   let currentQuestion = 0;
 
-  let correctAnswers = [
+  const correctAnswers = [
     "onclick",
     "JSON.stringify()",
     "A programming API for HTML and XML documents"
   ]
 
   function checkAndContinue(event) {
-    let answer = event.currentTarget.querySelector("p").textContent;
+    const answer = event.currentTarget.querySelector("p").textContent;
 
     if (answer === correctAnswers[currentQuestion]) {
         score++;
     }
 
-    let currentSection = event.currentTarget.parentElement.parentElement;
+    const currentSection = event.currentTarget.parentElement.parentElement;
 
     currentQuestion++;
 
-    if (currentQuestion === correctAnswers.length) {
-        currentSection.style.display = "none";
+    currentSection.style.display = "none";
 
-        document.getElementById("results").style.display = "block";
+    if (currentQuestion === correctAnswers.length) {      
+      document.getElementById("results").style.display = "block";
 
-        if (score === correctAnswers.length) {
-            document.querySelector("#results h1").textContent =
+      if (score === correctAnswers.length) {
+        document.querySelector("#results h1").textContent =
                 "You are recognized as top JavaScript fan!";
-        } else {
-            document.querySelector("#results h1").textContent =
+      } else {
+        document.querySelector("#results h1").textContent =
                 `You have ${score} right answers`;
-        }
+      }
     } else {
-        let nextSection = currentSection.nextElementSibling;
-
-        currentSection.style.display = "none";
-        nextSection.style.display = "block";
+      const nextSection = currentSection.nextElementSibling;
+      nextSection.style.display = "block";
     }
   }
 }
