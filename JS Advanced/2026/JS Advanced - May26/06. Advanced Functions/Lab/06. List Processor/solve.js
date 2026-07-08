d8b30d2da13343c31f1613e3adf49169ab12a00a
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-08 23:26:09 +0300

    Update solve.js

---
+++ JS Advanced/2026/JS Advanced - May26/06. Advanced Functions/Lab/06. List Processor/solve.js
@@ -0,0 +1,22 @@
+function solve(input) {
+    let result = [];
+    const commands = {
+        add(string) {
+            result.push(string);
+        },
+        remove(string) {
+            result = result.filter(x => x !== string);
+        },
+        print() {
+            console.log(result.join(','));
+            
+        }
+    }
+    input.forEach((element) => {
+        let [command, value] = element.split(" ");
+        commands[command](value);
+    })
+    
+}
+
+solve(['add hello', 'add again', 'remove hello', 'add again', 'print']);
