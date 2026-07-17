b799085ca498d7a7ee2386620a19e32b94da4bbf
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-17 14:33:52 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/06. Advanced Functions/Exercise/07. Simple Calculator/app.js
+++ JS Advanced/2026/JS Advanced - May26/06. Advanced Functions/Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,28 @@
 function calculator() {
-    // TODO:
+    let num1;
+    let num2;
+    let result;
+    return {
+        init(number1, number2, resField) {
+            num1 = document.querySelector(number1);
+            num2 = document.querySelector(number2);
+            result = document.querySelector(resField);
+        },
+        add() {
+            const first = Number(num1.value);
+            const second = Number(num2.value);
+            result.value = first + second;
+        },
+        subtract() {
+            const first = Number(num1.value);
+            const second = Number(num2.value);
+            result.value = first - second;
+        }
+    }
 }
 
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
 
 
 
