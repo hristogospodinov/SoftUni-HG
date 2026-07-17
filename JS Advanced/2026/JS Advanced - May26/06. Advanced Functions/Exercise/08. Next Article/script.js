aa7e8a4f963ddb47238b016b2f181668d17529f4
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-17 20:49:10 +0300

    Update script.js

--- JS Advanced/2026/JS Advanced - May26/06. Advanced Functions/Exercise/08. Next Article/script.js
+++ JS Advanced/2026/JS Advanced - May26/06. Advanced Functions/Exercise/08. Next Article/script.js
@@ -1,3 +1,19 @@
 function getArticleGenerator(articles) {
-    // TODO
+    
+    return function() {
+        if (articles.length === 0) {
+            return;
+        }
+        const text = articles.shift();
+        const article = document.createElement('article');
+        article.textContent = text;
+        document.getElementById('content').appendChild(article);
+    }
 }
+
+
+
+showNext();
+showNext();
+showNext();
+showNext();
