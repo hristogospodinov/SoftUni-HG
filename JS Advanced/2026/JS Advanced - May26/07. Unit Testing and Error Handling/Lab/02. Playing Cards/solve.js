b42e8674eb8f9e808c9a5f7f8c198ad56aaf0b8d
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-21 16:59:01 +0300

    Update solve.js

--- JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Lab/02. Playing Cards/solve.js
+++ JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Lab/02. Playing Cards/solve.js
@@ -1,21 +1,38 @@
 function playingCards(face, suit) {
-    let card = {
-        face,
-        suit,
-        toString() {
-            return `${this.face}${suits[this.suit]}`;
-        }
-    }
     let suits = {
         S: '\u2660',
         H: '\u2665',
         D: '\u2666',
         C: '\u2663'
     }
+    const validFaces = [
+        '2', '3', '4', '5', '6', '7',
+        '8', '9', '10', 'J', 'Q', 'K', 'A'
+    ];
+
+    if (!validFaces.includes(face)) {
+        throw new Error('Invalid card!');
+    }
+
+    if (!suits.hasOwnProperty(suit)) {
+        throw new Error('Invalid card!');
+    }
+
+    let card = {
+        face,
+        suit,
+        toString() {
+            return `${this.face}${suits[this.suit]}`;
+        }
+    }   
+
+    return card;
 }
 
 let card1 = playingCards('A', 'S');
 let card2 = playingCards('K', 'H');
 
-card1.toString();
-card2.toString();
+console.log(card1.toString());
+console.log(String(card1));
+
+console.log(card2.toString());
