28d462b4bad8cb5e8c569b58f96fab2a5f59f8cf
hristo-W3 <hristo_valeriev@yahoo.com>
2026-07-30 19:09:15 +0300

    Update app.js

--- JS Advanced/2026/JS Applications - Jun26/02. Asynchronous Programming/Lab/02.Github-Repos/scripts/app.js
+++ JS Advanced/2026/JS Applications - Jun26/02. Asynchronous Programming/Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,41 @@
 function loadRepos() {
-	console.log("TODO...");
+	// read input value from input id=username
+	let userNameInput = document.getElementById('username');
+	let userName = userNameInput.value;
+
+	// clear current list
+	let result = document.getElementById('repos');
+	result.replaceChildren();
+
+	// get user repos
+	// if request is successful
+	//     display repos
+	// else
+	//     display error
+	fetch(`https://api.github.com/users/${userName}/repos`)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(response.status);
+			}
+			return response.json();
+		})
+		.then(repos => {
+			repos.forEach(repo => {
+				let li = document.createElement("li");
+				let a = document.createElement('a');
+				a.textContent = repo.full_name;
+				a.href = repo.html_url;
+				a.target='_blank';
+				li.appendChild(a);
+				result.appendChild(li);
+			})
+		})
+		.catch(error => {
+			let li = document.createElement("li");
+			li.textContent = error;
+			result.appendChild(li);
+		});
+
+
+
 }
