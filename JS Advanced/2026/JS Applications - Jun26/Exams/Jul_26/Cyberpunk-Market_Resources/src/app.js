7dc918a5935bb911c860b4ff7a5efac155030338
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-01 09:50:55 +0300

    Update app.js

--- JS Advanced/2026/JS Applications - Jun26/Exams/Jul_26/Cyberpunk-Market_Resources/src/app.js
+++ JS Advanced/2026/JS Applications - Jun26/Exams/Jul_26/Cyberpunk-Market_Resources/src/app.js
@@ -1,1 +1,9 @@
+import { homeView } from './views/home.js';
+import page from 'page';
+import { render } from 'lit-html';
+
 const main = document.getElementById('main-element');
+
+page('/', homeView);
+
+page.start()
