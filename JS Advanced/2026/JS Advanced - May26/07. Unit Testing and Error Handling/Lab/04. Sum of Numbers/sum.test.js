f034d3cb6ba3f27e7f7a89e590b841c960f900b8
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-03 15:05:45 +0300

    Update sum.test.js

--- JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Lab/04. Sum of Numbers/sum.test.js
+++ JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Lab/04. Sum of Numbers/sum.test.js
@@ -1,10 +1,26 @@
-const sum = require('./solve.js');
+import sum from './solve.js';
 
-const { expect } = require('chai');
+import { expect } from 'chai';
 
 describe('sum', function () {
     it('test if [1,2,3] equals 6', function () {
         let result = sum([1,2,3]);
         expect(result).to.equal(6);
+    });
+    it('test if [] equals 0', function () {
+        let result = sum([]);
+        expect(result).to.equal(0);
+    });
+        it('test if [-2,-3] equals -5', function () {
+        let result = sum([-2,-3]);
+        expect(result).to.equal(-5);
+    });
+    it('test if [0.1,0.2] equals 0.3', function () {
+        let result = sum([0.1,0.2]);
+        expect(result).to.be.closeTo(0.3, 0.0001);
+    });
+    it('test if [-2,0,5,0.1] equals 3.1', function () {
+        let result = sum([-2,0,5,0.1]);
+        expect(result).to.be.closeTo(3.1, 0.0001);
     });
 });
