be2fa9fa26db72418e1236c25c2f3aaf183643db
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-05 00:18:27 +0300

    Update app.js

--- JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
+++ JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let email = document.getElementById('email');
+    email.addEventListener('change', onChange);
+
+    function onChange(event) {
+        let input = event.currentTarget.value;
+        let regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        if (!regex.test(input)) {
+            email.classList.add("error");
+        } else {
+            email.classList.remove('error');
+        }
+    }
 }
