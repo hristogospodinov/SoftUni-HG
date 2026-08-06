3dbc1618a61c500d33a5ec9b1c08a1e630054b1b
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-06 13:25:49 +0300

    Update formValidation.js

--- JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Exercise/07.Form-Validation/formValidation.js
+++ JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Exercise/07.Form-Validation/formValidation.js
@@ -1,3 +1,27 @@
 function validate() {
-    // TODO
+    let username = document.getElementById("username");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let confirmPassword = document.getElementById("confirm-password");
+    let companyCheckbox = document.getElementById("company");
+    companyCheckbox.addEventListener('change', onChange);
+    let companyFieldset = document.getElementById("companyInfo");
+    let companyNumber = document.getElementById("companyNumber");
+    let isValid = document.getElementById("valid");
+    let form = document.getElementById("registerForm");
+    form.addEventListener('submit', onSubmit);
+
+    function onChange(event) {
+        companyFieldset.style.display = event.currentTarget.checked ? "block" : "none";
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        let usernameRegex = /^[A-Za-z0-9]{3,20}$/;
+        if(!usernameRegex.test(username)) {
+            username.style.borderColor = 'red';
+        } else {
+            username.style.borderColor = 'none';
+        }
+    }
 }
