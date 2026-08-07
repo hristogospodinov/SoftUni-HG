df6e93e8f682480ae79ffa445c706acbc4155716
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-07 14:54:14 +0300

    Update formValidation.js

--- JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Exercise/07.Form-Validation/formValidation.js
+++ JS Advanced/2026/JS Advanced - May26/07. Unit Testing and Error Handling/Exercise/07.Form-Validation/formValidation.js
@@ -1,27 +1,85 @@
 function validate() {
-    let username = document.getElementById("username");
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-    let confirmPassword = document.getElementById("confirm-password");
-    let companyCheckbox = document.getElementById("company");
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm-password');
+
+    const companyCheckbox = document.getElementById('company');
+    const companyInfo = document.getElementById('companyInfo');
+    const companyNumber = document.getElementById('companyNumber');
+
+    const validDiv = document.getElementById('valid');
+    const form = document.getElementById('registerForm');
+
     companyCheckbox.addEventListener('change', onChange);
-    let companyFieldset = document.getElementById("companyInfo");
-    let companyNumber = document.getElementById("companyNumber");
-    let isValid = document.getElementById("valid");
-    let form = document.getElementById("registerForm");
     form.addEventListener('submit', onSubmit);
 
-    function onChange(event) {
-        companyFieldset.style.display = event.currentTarget.checked ? "block" : "none";
+    function onChange() {
+        companyInfo.style.display = companyCheckbox.checked
+            ? 'block'
+            : 'none';
     }
 
     function onSubmit(event) {
         event.preventDefault();
-        let usernameRegex = /^[A-Za-z0-9]{3,20}$/;
-        if(!usernameRegex.test(username)) {
+
+        let isFormValid = true;
+
+        const usernameRegex = /^[A-Za-z0-9]{3,20}$/;
+        const passwordRegex = /^\w{5,15}$/;
+        const emailRegex = /^.*@.*\..*$/;
+
+        // Username
+        if (!usernameRegex.test(username.value)) {
             username.style.borderColor = 'red';
+            isFormValid = false;
         } else {
-            username.style.borderColor = 'none';
+            username.style.border = 'none';
         }
+
+        // Password
+        if (!passwordRegex.test(password.value)) {
+            password.style.borderColor = 'red';
+            isFormValid = false;
+        } else {
+            password.style.border = 'none';
+        }
+
+        // Confirm Password
+        if (!passwordRegex.test(confirmPassword.value)) {
+            confirmPassword.style.borderColor = 'red';
+            isFormValid = false;
+        } else {
+            confirmPassword.style.border = 'none';
+        }
+
+        // Passwords match
+        if (password.value !== confirmPassword.value) {
+            password.style.borderColor = 'red';
+            confirmPassword.style.borderColor = 'red';
+            isFormValid = false;
+        }
+
+        // Email
+        if (!emailRegex.test(email.value)) {
+            email.style.borderColor = 'red';
+            isFormValid = false;
+        } else {
+            email.style.border = 'none';
+        }
+
+        // Company
+        if (companyCheckbox.checked) {
+            let number = Number(companyNumber.value);
+
+            if (number < 1000 || number > 9999) {
+                companyNumber.style.borderColor = 'red';
+                isFormValid = false;
+            } else {
+                companyNumber.style.border = 'none';
+            }
+        }
+
+        validDiv.style.display = isFormValid ? 'block' : 'none';
     }
 }
