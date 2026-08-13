1ae80445f20b1567530babab496bd9bb4b66f0d5
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-14 00:07:00 +0300

    Update solve.js

--- JS Advanced/2026/JS Advanced - May26/08. Classes/Lab/03. Circle/solve.js
+++ JS Advanced/2026/JS Advanced - May26/08. Classes/Lab/03. Circle/solve.js
@@ -15,3 +15,12 @@ class Circle {
         return Math.PI * this.radius ** 2;
     }
 }
+
+let c = new Circle(2);
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
+c.diameter = 1.6;
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
