84d06e28dda2b0b0de7e05cc795e48cc5ce906a6
hristo-W3 <hristo_valeriev@yahoo.com>
2026-08-13 17:34:07 +0300

    Update solve.js

--- JS Advanced/2026/JS Advanced - May26/08. Classes/Lab/02. Get Persons/solve.js
+++ JS Advanced/2026/JS Advanced - May26/08. Classes/Lab/02. Get Persons/solve.js
@@ -10,3 +10,15 @@ class Person {
         return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
     }
 }
+
+function getPersons() {
+    let persons = [];
+    persons.push(new Person("Anna", "Simpson", 22, "anna@yahoo.com"));
+    persons.push(new Person("SoftUni"));
+    persons.push(new Person("Stephan", "Johnson", 25));
+    persons.push(new Person("Gabriel", "Peterson", 24, "g.p@gmail.com"));
+    
+    return persons;
+}
+
+getPersons();
